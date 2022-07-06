3a6e155a827cdb8deabf5e6b0dc0833f3f6354ca
João Elias Maccari <joaomaccari@hotmail.com>
2022-07-05 21:52:54 -0300

    validção

--- js/script.js
+++ js/script.js
@@ -2,33 +2,64 @@ var vendas = document.querySelectorAll('.venda')
 let somaTot6f = 0
 let somaTot9f = 0
 
-for (let i =0; i<vendas.length; i++) {
+for (let i = 0; i<vendas.length; i++) {
     
-    var venda = vendas[i]
+    let venda = vendas[i]
 
+    let tdNome = venda.querySelector(".info-cliente");
     let tdQt = venda.querySelector('.info-qtd');
-    let quantidade = Number(tdQt.textContent)
-
     let tdTipo = venda.querySelector('.info-tipo')
-    let tipo = tdTipo.textContent
-
-    let tdTotal = venda.querySelector('.info-total')
-    let total = tdTotal.textContent
-
     let tdSocio = venda.querySelector('.info-socio')
+    let tdTotal = venda.querySelector('.info-total')
+
+    let nome = tdNome.textContent
+    let quantidade = Number(tdQt.textContent)
+    let tipo = tdTipo.textContent
+    let total = tdTotal.textContent
     let socio = tdSocio.textContent
 
-    console.log(tipo);
+    
+    //let campoEhValido = true 
+
+    if (quantidade <= 0 ) {
+        //campoEhValido = false
+        tdQt.textContent = "Insira um valor correto"
+        venda.classList.add("campo-invalido")
+    }
+
+    if (nome == "") {
+        venda.classList.add("campo-invalido")
+        tdNome.textContent = "Insira um nome"
+        
+    }
+
+    if (tipo == "") {
+        venda.classList.add("campo-invalido")
+        tdTipo.textContent = "Insira um tipo"
+
+    }
+
+    if (total == "") {
+        venda.classList.add("campo-invalido")
+        tdTotal.innerHTML = "Insira o total"
+    }
+
+    if (socio == "") {
+        venda.classList.add("campo-invalido")
+        tdSocio.textContent = "Insira um nome"
+    }
     
 
-    if (tipo == '6 Furos') {
-        somaTot6f += quantidade
-    }
+    if(tipo == '6 Furos') {
+            somaTot6f += quantidade
+        } else if (tipo == "9 Furos") {
+            somaTot9f += quantidade
+        }
 
-    if (tipo == '9 Furos'){
-        somaTot9f += quantidade
-    }
+   
+        
 }
+
 console.log(somaTot6f)
 console.log(somaTot9f)
 
